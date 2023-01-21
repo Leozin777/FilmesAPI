9f60a3cf802fb91127346b8d15b0b80dfef514d3
leozin777 <leo.cardoso@rede.ulbra.br>
2023-01-21 00:08:04 -0300

    git ignore add

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -5,10 +5,16 @@ namespace FilmesAPI.Models
     public class Filme
     {
         [Required(ErrorMessage = "O título do filme é obrigatório")]
+        [StringLength(100)]
         public string Titulo { get; set; }
+
         [Required(ErrorMessage = "O genêro do filme é obrigatório")]
         [MaxLength(50, ErrorMessage = "O gênero pode ter no máximo 50 caracteres")]
         public string Genero { get; set; }
+
+        [Required(ErrorMessage = "O diretor é obrigatório")]
+        public string Diretor { get; set; }
+
         [Required]
         [Range(70, 600, ErrorMessage = "A duração deve ser de 70 a 600 minutos")]
         public int Duracao { get; set; }
