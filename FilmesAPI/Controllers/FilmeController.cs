9bbd796b9495aa0b37bd252372f1a64ec3e3e349
leozin777 <leo.cardoso@rede.ulbra.br>
2023-01-21 00:04:29 -0300

    Segunda aula finalizada

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> Filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme p_filme)
+    {
+        Filmes.Add(p_filme);
+        Console.WriteLine(p_filme.Titulo);
+        Console.WriteLine(p_filme.Duracao);
+    }
+}
